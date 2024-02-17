c8490ae2326840d93f3878e4bba1d0107dd6e3c7
anyars-encarta <anyarsencarta@gmail.com>
2024-02-17 10:44:29 +0000

    Remove header styling from App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header"> */}
         <ModalUse />
-      </header>
+      {/* </header> */}
     </div>
   );
 }
