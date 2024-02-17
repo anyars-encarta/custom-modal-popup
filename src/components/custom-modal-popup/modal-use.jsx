1fda4e7476ec7632122d85dabe746aa2b81b0a1d
anyars-encarta <anyarsencarta@gmail.com>
2024-02-17 12:22:34 +0000

    Install and correct linter errors

--- src/components/custom-modal-popup/modal-use.jsx
+++ src/components/custom-modal-popup/modal-use.jsx
@@ -1,35 +1,34 @@
-import { useState } from "react";
-import Modal from "./modal";
+import { useState } from 'react';
+import Modal from './modal';
 import './styles.css';
 
 const ModalUse = () => {
-    const [showModalPopup, setShowModalPopup] = useState(false);
+  const [showModalPopup, setShowModalPopup] = useState(false);
 
-    const handleToggleModalPopup = () => {
-        setShowModalPopup(!showModalPopup);
-    }
+  const handleToggleModalPopup = () => {
+    setShowModalPopup(!showModalPopup);
+  };
 
-    const onClose = () => {
-        setShowModalPopup(false);
-    }
+  const onClose = () => {
+    setShowModalPopup(false);
+  };
 
-    return (
-        <div>
-            <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
+  return (
+    <div>
+      <button type="button" onClick={handleToggleModalPopup}>Open Modal Popup</button>
 
-            {
+      {
                 showModalPopup && (
-                    <Modal
-                        id={'custom-id'}
-                        onClose={onClose}
-                        header={<h1>Customized Header</h1>}
-                        body={<div>Customized body</div>}
-                        footer={<h2>Customized Footer</h2>}
-                    />
+                <Modal
+                  onClose={onClose}
+                  header={<h1>Customized Header</h1>}
+                  body={<div>Customized body</div>}
+                  footer={<h2>Customized Footer</h2>}
+                />
                 )
             }
-        </div>
-    )
+    </div>
+  );
 };
 
 export default ModalUse;
